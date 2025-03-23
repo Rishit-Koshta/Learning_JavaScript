550a294420b208cf2e2c1f1d419cf1a28a3cd059
Rishit-Koshta <koshtarishit@gmail.com>
2025-03-23 15:43:29 +0530

    learned variable declaration

---
+++ variable_declaration/script.js
@@ -0,0 +1,33 @@
+//let a = "Rishit";
+//console.log(a);
+
+//let b = 5;
+//let c = 10;
+
+//console.log(b+c);
+
+//const obj ={
+  //  name:"Rishit",
+  //  dob:23_01_2005,
+  //  age:20
+//};
+
+//console.log(obj);
+//console.log(obj.name);
+
+const x = -1;
+const y = "-1";
+
+console.log(x==y);
+console.log(x===y);
+
+const o = 4;
+const p = "name";
+const d = true;
+
+console.log(typeof o);
+console.log(typeof p);
+console.log(typeof d);
+
+let z =  x == -1?"hello":"bye";
+console.log(z);
