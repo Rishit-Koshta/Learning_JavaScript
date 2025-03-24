074fdca1b106aef5f362377dfd32d96bfb51cae2
Rishit-Koshta <koshtarishit@gmail.com>
2025-03-24 16:08:54 +0530

    learn  about if and else conditiion

---
+++ if_else_statement/script.js
@@ -0,0 +1,17 @@
+let a = 18;
+let b = 17;
+let c = 19;
+
+if(a>b&& a>c){
+    console.log("a is greater");
+}
+else if(b>a&&b>c){
+    console.log("b is greater than a");
+}
+
+else if(c>a&&c>b){
+    console.log("c is greatest");
+}
+else{
+    console.log("everyone is equal");
+}
